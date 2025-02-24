35c0fb5959818c36102468534309d83b54fcb88a
Ruhan Nandalal <ruhannandalal.in@gmail.com>
2025-02-25 00:07:50 +0530

    "Enable static export in Next.js"

--- fullstack/next.config.js
+++ fullstack/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Enable static export
   images: {
     domains: ["images.unsplash.com", "i.imgur.com"],
+    unoptimized: true, // Disable image optimization for static export
   },
 };
 
